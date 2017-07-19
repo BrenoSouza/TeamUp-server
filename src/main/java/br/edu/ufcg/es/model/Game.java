df34bc55bdc787c90467f788449bfa7b19354480
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2017-07-18 22:18:20 -0300

    Ajustes na Classe Game

--- src/main/java/br/edu/ufcg/es/model/Game.java
+++ src/main/java/br/edu/ufcg/es/model/Game.java
@@ -1,6 +1,8 @@
 package br.edu.ufcg.es.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -33,16 +35,66 @@ public class Game {
 	private long idOwner;
 	
 	@Column
+	private List<Long> invites;
 	
 	public Game(Date date, String local, String sport, String description){
 		this.date = date;
 		this.local = local;
 		this.sport = local;
 		this.description = description;
+		this.invites = new ArrayList<>();
 	}
 	
 	public Game(){
 		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public long getIdOwner() {
+		return idOwner;
+	}
+
+	public void setIdOwner(long idOwner) {
+		this.idOwner = idOwner;
+	}
+
+	public List getInvites() {
+		return invites;
+	}
+
+	public void setInvites(List invites) {
+		this.invites = invites;
 	}
 	
 	
